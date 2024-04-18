0829ad0b8776d9e64f54c94e03753d6597801012
mike <a3804430@hotmail.com>
2024-04-18 14:36:08 +0800

    feat: add watch demo

---
+++ 04/vue-watch-demo/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DemoComponent from "./components/04-no-watch-2.vue";
+</script>
+<template>
+  <div>
+    <DemoComponent />
+  </div>
+</template>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ 04/vue-watch-demo/src/components/01-watch.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const name = ref("Jacky");
+
+const useInfo = ref({
+  age: 12,
+  name: "Mike",
+  address: "Taipei",
+});
+
+setTimeout(() => {
+  name.value = "Mike";
+  useInfo.value.name = "Jacky";
+}, 2000);
+
+// 監聽 name
+watch(name, (newVal, oldVal) => {
+  console.log({ newVal, oldVal });
+});
+
+/**
+ * immediate: true 初始化先觸發一次
+ * deep: true 深度監聽
+ */
+watch(
+  useInfo,
+  (newInfo) => {
+    console.log("deep=>", newInfo);
+  },
+  {
+    deep: true,
+    immediate: true,
+    once: true,
+  }
+);
+
+// once 只會觸發一次
+watch(
+  name,
+  (newName) => {
+    console.log("once=>", newName);
+  },
+  { once: true }
+);
+</script>
+<template>
+  <div></div>
+</template>
+<style></style>

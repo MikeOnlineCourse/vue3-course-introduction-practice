3ac2a684be8987be676ffdfe339d0989308cf883
MikeCheng1208 <a3804430@hotmail.com>
2024-07-31 20:07:30 +0800

    fix: fix demo

--- exercise/13-typescript/vue3-typescript-demo/src/components/06-props.vue
+++ exercise/13-typescript/vue3-typescript-demo/src/components/06-props.vue
@@ -1,10 +1,25 @@
 <script setup>
+//   name: "Mike",
+//   age: 12,
+//   info: {
+//     email: "1208966@gmail.com",
+//     phone: "0910-xxx-xxx",
+//   },
 const props = defineProps({
-  name: string,
-  age: number,
+  name: {
+    type: String,
+    default: "Mike",
+  },
+  age: {
+    type: Number,
+    default: 12,
+  },
   info: {
-    email: string,
-    phone: string,
+    type: Object,
+    default: () => ({
+      email: "1208966@gmail.com",
+      phone: "0910-xxx-xxx",
+    }),
   },
 });
 </script>
